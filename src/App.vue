73fd1422f056ec0779a78172d037fc1d278d44db
张瑞 <2896402717@qq.com>
2025-02-19 09:36:01 +0800

    准备部署

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Header></Header>
-  <router-view></router-view>
-  <Footer></Footer>
-</template>
-<script setup lang="ts"></script>
-<style scoped></style>
--- src/types/MyIcon.ts
+++
@@ -1,6 +0,0 @@
-declare interface IconMap {
-  [key: string]: string
-  html: string
-  css: string
-  js: string
-}
