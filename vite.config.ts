a257e7d10fb37e82ca8faae3c17332ac84b9e55c
雨溪 <84778445+yuxi-ovo@users.noreply.github.com>
2026-01-05 21:28:06 +0800

    去掉打包结果分析插件配置

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import MotionResolver from 'motion-v/resolver'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,13 +24,13 @@ export default defineConfig({
     Components({
       dts: true,
       resolvers: [MotionResolver()]
-    }),
-    visualizer({
-      open: true, // 打包完成后自动在浏览器打开分析报告
-      filename: 'stats.html', // 生成的分析文件名称
-      gzipSize: true, // 展示 gzip 压缩后的大小
-      brotliSize: true // 展示 brotli 压缩后的大小
     })
+    // visualizer({
+    //   open: true, // 打包完成后自动在浏览器打开分析报告
+    //   filename: 'stats.html', // 生成的分析文件名称
+    //   gzipSize: true, // 展示 gzip 压缩后的大小
+    //   brotliSize: true // 展示 brotli 压缩后的大小
+    // })
   ],
   resolve: {
     alias: {
